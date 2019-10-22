39a1a4da00debb4c748e3f26bcce078952ec8d4b
Cesar Santos <cesaraugusto.santos@outlook.com>
2019-10-22 01:26:37 -0300

    Corrigindo o backend com cors

--- App/src/app/data.service.ts
+++ App/src/app/data.service.ts
@@ -6,11 +6,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class DataService {
 
-  public baseUrl = "htpp://localhost:5000/QuizFATEC";
+  //public baseUrl = "https://345362ce.ngrok.io/QuizFATEC";
+  public baseUrl = "http://localhost:5000/QuizFATEC";
 
   constructor(private http: HttpClient) { }
 
   public getAuthenticated(data: any) {
+    console.log(data)
     return this.http.post(`${this.baseUrl}/Usuarios/Login`, data);
   }
 
